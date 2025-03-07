74bdbee0e8edf53a1d1ee5c19f3cd1d906efd68f
liming <liming_longxi@163.com>
2025-03-07 10:54:51 +0800

    【add】新增二维矩阵的计算

--- src/mat2.js
+++ src/mat2.js
@@ -1,4 +1,4 @@
-import { ARRAY_TYPE } from "./common";
+import { ARRAY_TYPE, equals } from "./common";
 
 export default class Mat2 {
   _value = [];
@@ -18,11 +18,11 @@ export default class Mat2 {
 
   static fromValues(m00, m01, m10, m11) {
     const result = new Mat2();
-    this.set(m00, m01, m10, m11)
+    this.set(m00, m01, m10, m11);
     return result;
   }
 
-  set(m00, m01, m10, m11){
+  set(m00, m01, m10, m11) {
     this.set00(m00);
     this.set01(m01);
     this.set10(m10);
@@ -77,17 +77,112 @@ export default class Mat2 {
     this.set11(mat2.get11());
   }
 
-  identity(){
+  identity() {
     this.set00(1);
     this.set01(0);
     this.set10(0);
     this.set11(1);
   }
 
-  transpose(){
+  transpose() {
     const m01 = this.get01();
     const m10 = this.get10();
-    this.set01(m10)
-    this.set10(m01)
+    this.set01(m10);
+    this.set10(m01);
+  }
+
+  invert() {
+    let det = this.determinant();
+
+    if (!det) {
+      return null;
+    }
+    det = 1 / det;
+    const result = new Mat2();
+    result.set(
+      this.get00() / det,
+      -this.get01() / det,
+      -this.get10() / det,
+      this.get11() / det
+    );
+    return result;
+  }
+
+  adjugate() {
+    const result = new Mat2();
+    result.set(
+      this.get11() / det,
+      -this.get01() / det,
+      -this.get10() / det,
+      this.get00() / det
+    );
+    return result;
+  }
+
+  determinant() {
+    return this.get00() * this.get11() - this.get01() * this.get10();
+  }
+
+  multiply(mat2) {
+    const m00 = this.get00() * mat2.get00() + this.get10() * mat2.get01();
+    const m01 = this.get00() * mat2.get10() + this.get01() * mat2.get11();
+    const m02 = this.get10() * mat2.get00() + this.get11() * mat2.get10();
+    const m03 = this.get10() * mat2.get01() + this.get11() * mat2.get11();
+    const result = new Mat2();
+    result.set(m00, m01, m02, m03);
+    return result;
+  }
+
+  rotate(rad) {
+    let s = Math.sin(rad);
+    let c = Math.cos(rad);
+    const m00 = this.get00() * c + this.get10() * s;
+    const m01 = this.get01() * c + this.get11() * s;
+    const m02 = this.get00() * s + this.get10() * c;
+    const m03 = this.get01() * s + this.get11() * c;
+    this.set(m00, m01, m02, m03);
+  }
+
+  scale(vec2) {
+    this.set(
+      this.get00() * vec2.get("x"),
+      this.get01() * vec2.get("x"),
+      this.get10() * vec2.get("y"),
+      this.get11() * vec2.get("y")
+    );
+  }
+
+  add(mat2) {
+    const m00 = this.get00() + mat2.get00();
+    const m01 = this.get01() + mat2.get01();
+    const m02 = this.get10() + mat2.get10();
+    const m03 = this.get11() + mat2.get11();
+    this.set(m00, m01, m02, m03);
+  }
+
+  subtract(mat2) {
+    const m00 = this.get00() - mat2.get00();
+    const m01 = this.get01() - mat2.get01();
+    const m02 = this.get10() - mat2.get10();
+    const m03 = this.get11() - mat2.get11();
+    this.set(m00, m01, m02, m03);
+  }
+
+  exactEquals(mat2){
+    return this.get00() === mat2.get00() && this.get00() === mat2.get00() && this.get00() === mat2.get00() && this.get00() === mat2.get00()
+  }
+
+  equals(mat2) {
+    return (
+      equals(this.get00(), mat2.get00()) &&
+      equals(this.get01(), mat2.get01()) &&
+      equals(this.get10(), mat2.get10()) &&
+      equals(this.get11(), mat2.get11())
+    );
+  }
+
+
+  toString() {
+    return `Mat2(${this.get00()},${this.get01()},${this.get10()},${this.get11()})`;
   }
 }
